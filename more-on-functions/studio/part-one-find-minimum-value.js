92ac5925637d491ad19588e44b484433b17b7a03
vSutherland <142331739+vSutherland@users.noreply.github.com>
2023-09-29 09:53:55 -0500

    Studio Done

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -1,10 +1,25 @@
-//1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
-
-//Sample arrays for testing:
-let nums1 = [5, 10, 2, 42];
-let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
-let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
-
-//Using one of the test arrays as the argument, call your function inside the console.log statement below.
-
-console.log(/* your code here */);
+function findMinimumValue(arr) {
+    if (arr.length === 0) {
+      return undefined; // Handle the case of an empty array
+    }
+  
+    let minValue = arr[0]; // Initialize minValue with the first element of the array
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < minValue) {
+        minValue = arr[i]; // Update minValue if a smaller value is found
+      }
+    }
+  
+    return minValue;
+  }
+  
+  // Sample arrays for testing:
+  let nums1 = [5, 10, 2, 42];
+  let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+  let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+  
+  // Using one of the test arrays as the argument, call your function inside the console.log statement below.
+  
+  console.log(findMinimumValue(nums1));
+  
--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -1,8 +1,25 @@
-//Sample arrays for testing:
+// Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-//Sort each array in ascending order.
+// Sort each array in ascending order
+nums1.sort((a, b) => a - b);
+nums2.sort((a, b) => a - b);
+nums3.sort((a, b) => a - b);
 
-//Sort each array in decending order.
+console.log("Ascending Order:");
+console.log("nums1:", nums1);
+console.log("nums2:", nums2);
+console.log("nums3:", nums3);
+
+// Sort each array in descending order
+nums1.sort((a, b) => b - a);
+nums2.sort((a, b) => b - a);
+nums3.sort((a, b) => b - a);
+
+console.log("\nDescending Order:");
+console.log("nums1:", nums1);
+console.log("nums2:", nums2);
+console.log("nums3:", nums3);
+
