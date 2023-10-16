caf0e79d54db40378acf53f18d80beaff25cd6e2
Vance <vancesutherland@hotmail.com>
2023-10-16 18:39:28 -0500

    yes

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,5 +1,23 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
 
+class CrewCandidate {
+    constructor(name, mass, scores) {
+      this.name = name;
+      this.mass = mass;
+      this.scores = scores;
+    }
+  }
+  
+  // Create objects for the candidates
+  const bubbaBear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+  const merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+  const gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
+  
+  // Use console.log to verify the objects
+  console.log(bubbaBear);
+  console.log(merryMaltese);
+  console.log(gladGator);
+  
 
 
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
