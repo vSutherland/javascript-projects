5e57861a4c5d2d24fec4b5c22ac0198a252d13cc
Vance <vancesutherland@hotmail.com>
2023-10-16 18:58:54 -0500

    Studio

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -8,20 +8,33 @@ class CrewCandidate {
     }
   }
   
-  // Create objects for the candidates
-  const bubbaBear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
-  const merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
-  const gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
+  const bear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+  const maltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+  const gator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
   
-  // Use console.log to verify the objects
-  console.log(bubbaBear);
-  console.log(merryMaltese);
-  console.log(gladGator);
+  console.log(bear);
+  console.log(maltese);
+  console.log(gator);
   
 
 
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 
-
+class CrewCandidate {
+    constructor(name, mass, scores) {
+      this.name = name;
+      this.mass = mass;
+      this.scores = scores;
+    }
+  
+    addScore(newScore) {
+      this.scores.push(newScore);
+    }
+  }
+  
+  bear.addScore(83);
+  
+  console.log("Bubba's Scores:", bear.scores);
+  
 
 //Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
