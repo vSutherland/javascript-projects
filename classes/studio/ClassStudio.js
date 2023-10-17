a3707dd68cbb3791fc9d18033762c5e2d14b5834
Vance <vancesutherland@hotmail.com>
2023-10-16 19:49:17 -0500

    Studio done

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,40 +1,80 @@
-//Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
-
 class CrewCandidate {
     constructor(name, mass, scores) {
-      this.name = name;
-      this.mass = mass;
-      this.scores = scores;
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
     }
-  }
-  
-  const bear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
-  const maltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
-  const gator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
-  
-  console.log(bear);
-  console.log(maltese);
-  console.log(gator);
-  
 
-
-//Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
-
-class CrewCandidate {
-    constructor(name, mass, scores) {
-      this.name = name;
-      this.mass = mass;
-      this.scores = scores;
-    }
-  
     addScore(newScore) {
-      this.scores.push(newScore);
+        this.scores.push(newScore);
     }
-  }
-  
-  bear.addScore(83);
-  
-  console.log("Bubba's Scores:", bear.scores);
-  
 
-//Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
+    calculateAverageScore() {
+        if (this.scores.length === 0) {
+            return 0;
+        }
+        const total = this.scores.reduce((sum, score) => sum + score, 0);
+        return total / this.scores.length;
+    }
+
+    shouldAdmit() {
+        const averageScore = this.calculateAverageScore();
+        return averageScore >= 90;
+    }
+
+    status() {
+        const averageScore = this.calculateAverageScore();
+
+        if (averageScore >= 90) {
+            return "Accepted";
+        } else if (averageScore >= 80) {
+            return "Reserve";
+        } else if (averageScore >= 70) {
+            return "Probationary";
+        } else {
+            return "Rejected";
+        }
+    }
+}
+
+// Create objects for the candidates
+const bubbaBear = new CrewCandidate("Bubba Bear", 135, [88, 85, 90]);
+const merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97]);
+const gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62]);
+
+// Add scores to candidates' records
+bubbaBear.addScore(83);
+merryMaltese.addScore(95);
+gladGator.addScore(80);
+
+// Get the status for each candidate
+const bubbaBearStatus = bubbaBear.status();
+const merryMalteseStatus = merryMaltese.status();
+const gladGatorStatus = gladGator.status();
+
+console.log(`${bubbaBear.name} earned an average test score of ${bubbaBear.calculateAverageScore()}% and has a status of ${bubbaBearStatus}.`);
+console.log(`${merryMaltese.name} earned an average test score of ${merryMaltese.calculateAverageScore()}% and has a status of ${merryMalteseStatus}.`);
+console.log(`${gladGator.name} earned an average test score of ${gladGator.calculateAverageScore()}% and has a status of ${gladGatorStatus}.`);
+
+// Reset Glad Gator's scores for the test
+gladGator.scores = [75, 78, 62];
+
+// Define the target status
+const targetStatus = "Accepted";
+
+let testsTaken = 0;
+
+while (gladGator.status() !== targetStatus) {
+    // Increment the test score by 5 (or any other increment you prefer)
+    const newScore = gladGator.scores[testsTaken % 3] + 5;
+    
+    // Ensure the score doesn't exceed 100
+    if (newScore <= 100) {
+        gladGator.scores[testsTaken % 3] = newScore;
+        testsTaken++;
+    } else {
+        break; // Break the loop if the score cannot be increased further
+    }
+}
+
+console.log(`Glad Gator reached ${targetStatus} status after taking ${testsTaken} tests.`);
